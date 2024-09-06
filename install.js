39f1125481efe6ea5703c17c783e50401904f83e
beWiZ <38848227+downlifted@users.noreply.github.com>
2024-09-06 13:23:09 -0400

    Add files via upload

--- install.js
+++ install.js
@@ -1,85 +1,44 @@
-const { virtual_env, project_dir } = require("./constants");
-const path = require('path');
-
-function getInstallCommand(kernel) {
-  const { platform, gpu } = kernel;
-
-  function combineLists(list1, list2) {
-    return [...list1, ...list2];
-  }
-
-  project_requirements = [
-    `pip install -r ${path.resolve(__dirname, project_dir, 'requirements.txt')}`,
-    `pip install -r ${path.resolve(__dirname, project_dir, 'comfy_runner', 'requirements.txt')}`,
-    `pip install -r ${path.resolve(__dirname, project_dir, 'ComfyUI', 'requirements.txt')}`,
-  ];
-
-  // only handling linux and win32 for now
-  if (platform == "linux") {
-    cmd_list = []; // pinokio by default uses py3.10
-    return combineLists(cmd_list, project_requirements);
-  }
-
-  if (platform == "win32") {
-    cmd_list = [
-      "python.exe -m pip install --upgrade pip",
-      "pip install websocket",
-      "pip install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu118",
-    ];
-
-    return combineLists(cmd_list, project_requirements);
-  }
-
-  // only installing the base app for the mac
-  return [
-    `pip install -r ${path.resolve(__dirname, project_dir, 'requirements.txt')}`,
-  ];
-}
-
-module.exports = async (kernel) => {
-  const config = {
-    run: [
-      {
-        method: "shell.run",
+module.exports = {
+  run: [
+    // Edit this step to customize the git repository to use
+    {
+      method: "shell.run",
+      params: {
+        message: [
+          "git clone https://github.com/downlifted/Groovy-StyleSuite app",
+        ]
+      }
+    },
+    // Delete this step if your project does not use torch
+    {
+      method: "script.start",
+      params: {
+        uri: "torch.js",
         params: {
-          message: [
-            `git clone --depth 1 -b main https://github.com/downlifted/Groovy-StyleSuite.git ${project_dir}`,
-          ],
-        },
-      },
-      {
-        method: "shell.run",
-        params: {
-          path: project_dir,
-          message: [
-            "git clone --depth 1 -b main https://github.com/piyushK52/comfy_runner",
-            "git clone https://github.com/comfyanonymous/ComfyUI.git",
-          ],
-        },
-      },
-      {
-        method: "shell.run",
-        params: {
-          path: project_dir,
-          venv: virtual_env,
-          message: getInstallCommand(kernel)
-        },
-      },
-      {
-        method: "fs.copy",
-        params: {
-          src: `${project_dir}/.env.sample`,
-          dest: `${project_dir}/.env`,
-        },
-      },
-          peers: [
-            "https://github.com/cocktailpeanutlabs/automatic1111.git",
-            "https://github.com/cocktailpeanutlabs/fooocus.git",
-            "https://github.com/cocktailpeanutlabs/forge.git"
-          ]
+          venv: "env",                // Edit this to customize the venv folder path
+          path: "app",                // Edit this to customize the path to start the shell from
+          xformers: true   // uncomment this line if your project requires xformers
         }
-      },
-    ],
-  };
-  return config;
-};
+      }
+    },
+    // Edit this step with your custom install commands
+    {
+      method: "shell.run",
+      params: {
+        venv: "env",                // Edit this to customize the venv folder path
+        path: "app",                // Edit this to customize the path to start the shell from
+        message: [
+          "pip install gradio devicetorch",
+          "pip install -r requirements.txt"
+	  "python app.py"
+        ]
+      }
+    },
+    {
+      method: "fs.link",
+      params: {
+        venv: "app/env"
+      }
+    }
+  ]
+}
