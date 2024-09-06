877e7afbfa62d36345c61e13f7bad91e99f8e8ef
beWiZ <38848227+downlifted@users.noreply.github.com>
2024-09-05 22:29:35 -0400

    Add files via upload

--- pinokio.js
+++ pinokio.js
@@ -1,30 +1,87 @@
-const { exec } = require('child_process');
-const path = require('path');
-
+const path = require('path')
 module.exports = {
-  runGroovyStyleSuite: function (input, callback) {
-    // Construct the command to run the Python script
-    const pythonScript = path.join(__dirname, 'app.py');  // Ensure 'app.py' is in the same directory
-    const command = `python ${pythonScript}`;
-
-    console.log('Executing command:', command);
-
-    // Execute the Python script
-    exec(command, (error, stdout, stderr) => {
-      if (error) {
-        console.error(`Error: ${error.message}`);
-        callback(`Error: ${error.message}`);
-        return;
+  version: "2.0",
+  title: "Groovy-Stylesuite",
+  description: "",
+  icon: "icon.png",
+  menu: async (kernel, info) => {
+    let installed = info.exists("app/env")
+    let running = {
+      install: info.running("install.js"),
+      start: info.running("start.js"),
+      update: info.running("update.js"),
+      reset: info.running("reset.js")
+    }
+    if (running.install) {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Installing",
+        href: "install.js",
+      }]
+    } else if (installed) {
+      if (running.start) {
+        let local = info.local("start.js")
+        if (local && local.url) {
+          return [{
+            default: true,
+            icon: "fa-solid fa-rocket",
+            text: "Open Web UI",
+            href: local.url,
+          }, {
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        } else {
+          return [{
+            default: true,
+            icon: 'fa-solid fa-terminal',
+            text: "Terminal",
+            href: "start.js",
+          }]
+        }
+      } else if (running.update) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Updating",
+          href: "update.js",
+        }]
+      } else if (running.reset) {
+        return [{
+          default: true,
+          icon: 'fa-solid fa-terminal',
+          text: "Resetting",
+          href: "reset.js",
+        }]
+      } else {
+        return [{
+          default: true,
+          icon: "fa-solid fa-power-off",
+          text: "Start",
+          href: "start.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Update",
+          href: "update.js",
+        }, {
+          icon: "fa-solid fa-plug",
+          text: "Install",
+          href: "install.js",
+        }, {
+          icon: "fa-regular fa-circle-xmark",
+          text: "Reset",
+          href: "reset.js",
+        }]
       }
-
-      if (stderr) {
-        console.error(`Error output: ${stderr}`);
-        callback(`Error output: ${stderr}`);
-        return;
-      }
-
-      console.log(`Script output: ${stdout}`);
-      callback(stdout);  // Return the output to whatever called this function
-    });
+    } else {
+      return [{
+        default: true,
+        icon: "fa-solid fa-plug",
+        text: "Install",
+        href: "install.js",
+      }]
+    }
   }
-};
+}
