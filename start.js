a2eaac9f07825e7a2753f4af537dffa86a8b1814
beWiZ <38848227+downlifted@users.noreply.github.com>
2024-09-05 22:52:44 -0400

    Update start.js

--- start.js
+++ start.js
@@ -28,7 +28,7 @@ module.exports = {
       method: "local.set",
       params: {
         // the input.event is the regular expression match object from the previous step
-        url: "localhost:8188"
+        url: "app.py"
       }
     }
   ]
